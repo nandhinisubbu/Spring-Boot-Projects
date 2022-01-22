63b0a3e6dd7654df7db53330e55fb256f4cc60de
nandhini.subramani <nandhini.subramani@thoughtworks.com>
2022-01-22 12:21:36 +0530

    Add class student and functionality to return student data in API

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,9 +1,13 @@
 package com.example.demo;
-
+import com.example.demo.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
 
 @SpringBootApplication
 @RestController
@@ -14,7 +18,15 @@ public class DemoApplication {
 	}
 
 	@GetMapping
-	public String hello(){
-		return "Hello World";
+	public List<Student> hello(){
+		return List.of(
+				new Student(
+						1L,
+						"Nandhini",
+						"nandhini@gmail.com",
+						LocalDate.of(2001, Month.AUGUST,26),
+						20
+				)
+		);
 	}
 }
